03619d316eff41a742350ede9235c9dca1edcdc5
Benjamin <benmmari92@gmail.com>
2013-07-19 22:37:27 +0200

    Configure servlet to work with simple input

--- src/com/work/DA.java
+++ src/com/work/DA.java
@@ -15,14 +15,14 @@ public class DA extends HttpServlet{
 		try {
 			checkInitialized();
 			System.out.println("working");
-			String text = "some text";
+			String text = request.getParameter("name");
 
 			response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
 			response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
 			response.getWriter().write(text);       // Write response body.	
 			Rete engine =(Rete)(getServletContext().getAttribute("engine"));
 
-			engine.executeCommand("(printout t " + "ben and kevin" + " crlf)");
+			engine.executeCommand("(printout t " + text + " crlf)");
 		} catch (JessException jess) {
 		
 		}
