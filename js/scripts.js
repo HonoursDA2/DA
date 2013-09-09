9aea08abf578a8c3fbf544a5048aeb5734854d8f
hallatedzi <hallatedzi@gmail.com>
2013-09-09 17:13:47 +0200

    We are one, we are united

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ blurvision = { id: "#blurvision", clicked: false };
 fatigue = { id: "#fatigue", clicked: false };
 weightloss = { id: "#weightloss", clicked: false };
 drymouth = { id: "#drymouth", clicked: false };
-vomiting = { id: "#vomiting", clicked: false };
+abdominalpain = { id: "#abdominalpain", clicked: false };
 neusea = { id: "#neusea", clicked: false };
 irritability = { id: "#irritability", clicked: false };
 yeast = { id: "#yeast", clicked: false };
@@ -18,131 +18,118 @@ hunger = { id: "#hunger", clicked: false };
 
 function clicked(symptom) {
 
-    if (!symptom.clicked) {
-        $(symptom.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
-        symptom.clicked = true;
-    }
-    else {
-        $(symptom.id).css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
-        symptom.clicked = false;
-    }
+	if (!symptom.clicked) {
+		$(symptom.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
+		symptom.clicked = true;
+	}
+	else {
+		$(symptom.id).css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
+		symptom.clicked = false;
+	}
 }
 
-<<<<<<< HEAD
-function diagnose() { //Comment this out
-    
-    if (thirst.clicked) {
-        document.getElementById("advisorH1").innerHTML = "Drink Something";
-=======
 function createArray() {
-    var submitArray = [thirst,urinating,itchiness,dysfunction,blurvision,fatigue,weightloss,drymouth,vomiting,abdomenalpain];
-    return submitArray;
+	var submitArray = [thirst,urinating,itchiness,dysfunction,blurvision,fatigue,weightloss,drymouth,abdominalpain];
+	return submitArray;
 }
 
 function countClicks() {
-    var count = 0;
-    var array = createArray();
-    for (var x=0;x<array.length;x++) {
-        if (array[x].clicked) {
-            count++;
-        }
->>>>>>> origin/master
-    }
-    return count;
+	var count = 0;
+	var array = createArray();
+	for (var x=0;x<array.length;x++) {
+		if (array[x].clicked) {
+			count++;
+		}
+	}
+	return count;
 }
 
 function submitSymptoms() {
-    var submitArray = createArray();
-    var counter = 0;
-    var count = countClicks();
-    for (var x=0;x<submitArray.length;x++) {
-        if (submitArray[x].clicked) {
-            var symptomObject = submitArray[x];
-            var symptomName = $(symptomObject.id).attr('value'); 
-            alert(symptomName + " " + count +" " +counter);
-            if (counter == (count - 1)) {
-                $.get('servlet',{symptoms:symptomName,process:"complete"});
-                x = submitArray.length;
-            } else {
-                $.get('servlet',{symptoms:symptomName,process:"incomplete"});
-                }
-            
-            counter++;
-        }
+	var submitArray = createArray();
+	var counter = 0;
+	var count = countClicks();
+	for (var x=0;x<submitArray.length;x++) {
+		if (submitArray[x].clicked) {
+			var symptomObject = submitArray[x];
+			var symptomName = $(symptomObject.id).attr('value'); 
+			alert(symptomName + " " + count +" " +counter);
+			if (counter == (count - 1)) {
+				$.get('servlet',{symptoms:symptomName,process:"complete"});
+				x = submitArray.length;
+			} else {
+				$.get('servlet',{symptoms:symptomName,process:"incomplete"});
+			}
 
-    }
+			counter++;
+		}
+
+	}
 }
 
 var count = 0;
 
 function revert()
 {
-    if (count == 1) {
-        name = document.getElementById('name').value; //Comment this out
-        $("#intro").fadeIn(0);
-        document.getElementById("advisorH1").innerHTML = "Hello " + name + " how may I help you with ?"; //Comment this out
-        count--;
-        $("#gender,#profile #male, #profile #female").fadeOut(0);
-        $("#button").css({"margin":"0 0 1.5% 1.5%"});
-        $("#backb").fadeOut();
-    }
+	if (count == 1) {
+		name = document.getElementById('name').value; //Comment this out
+		$("#intro").fadeIn(0);
+		document.getElementById("advisorH1").innerHTML = "Hello " + name + " how may I help you with ?"; //Comment this out
+		count--;
+		$("#gender,#profile #male, #profile #female").fadeOut(0);
+		$("#button").css({"margin":"0 0 1.5% 1.5%"});
+		$("#backb").fadeOut();
+	}
 }
 
 function confirm() {
 
-    if (count == 2) {
-        $("#profile").fadeOut();
-    }
-    if (count == 1){
-        $("#isDiabetic,#profile #yes,#profile #no").fadeIn(0);
-        $("#gender,#profile #male, #profile #female").fadeOut(0);
-        count++;
-        
-        
-    }
-    if (count == 0) {
-<<<<<<< HEAD
-        name = document.getElementById('name').value; //Comment this out
-        $("#intro").fadeOut(0);
-        document.getElementById("advisorH1").innerHTML = "Hello " + name + " how may I help you with ?"; //Comment this out
-=======
-        $("#intro").fadeOut();
->>>>>>> origin/master
-        count++;
-        $("#gender,#profile #male, #profile #female").fadeIn(0);
-        $("#button").css({"margin":"0 0 1.5% 250px"});
-        $("#backb").fadeIn();
-    }
+	if (count == 2) {
+		$("#profile").fadeOut();
+	}
+	if (count == 1){
+		$("#isDiabetic,#profile #yes,#profile #no").fadeIn(0);
+		$("#gender,#profile #male, #profile #female").fadeOut(0);
+		count++;
+
+
+	}
+	if (count == 0) {
+		$("#intro").fadeOut(0);
+		count++;
+		$("#gender,#profile #male, #profile #female").fadeIn(0);
+		$("#button").css({"margin":"0 0 1.5% 250px"});
+		$("#backb").fadeIn();
+	}
 }
 
 function gSpecific(gender) {//Comment this out
 
-    if (gender.id == "male") {
-        $("#vitchiness").fadeOut();
-        $("#dysfunction").fadeIn();
-        $("#" + gender.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
-        $("#female").css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
-    }
-    if (gender.id == "female") {
-        $("#dysfunction").fadeOut();
-        $("#vitchiness").fadeIn();
-        $("#" + gender.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
-        $("#male").css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
-    }
+	if (gender.id == "male") {
+		$("#vitchiness").fadeOut();
+		$("#dysfunction").fadeIn();
+		$("#" + gender.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
+		$("#female").css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
+	}
+	if (gender.id == "female") {
+		$("#dysfunction").fadeOut();
+		$("#vitchiness").fadeIn();
+		$("#" + gender.id).css({ "color": "rgba(255,0,0,0.7)", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
+		$("#male").css({ "color": "grey", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
+	}
 }
 
 function isDiabetic(answer) {
 
-    if (answer.id == "yes") {
-        $("#" + answer.id).css({ "color": "green", "border-bottom": "5px solid rgba(0,100,0,0.7)" });
-        $("#no").css({ "color": "white", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
-    }
-    if (answer.id == "no") {
-        $("#" + answer.id).css({ "color": "red", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
-        $("#yes").css({ "color": "white", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
-    }
+	if (answer.id == "yes") {
+		$("#" + answer.id).css({ "color": "green", "border-bottom": "5px solid rgba(0,100,0,0.7)" });
+		$("#no").css({ "color": "white", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
+	}
+	if (answer.id == "no") {
+		$("#" + answer.id).css({ "color": "red", "border-bottom": "5px solid rgba(255,0,0,0.7)" });
+		$("#yes").css({ "color": "white", "border-bottom": "0px solid rgba(255,0,0,0.7)" });
+	}
 }
 
 function splash() {
-    $("#splash").delay(2000).effect("puff", 500);
+	$("#splash").delay(2000).effect("puff", 500);
 }
