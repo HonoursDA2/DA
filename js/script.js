9f8300385e1f358c5111b53980d0063dde52de8b
Benjamin <benmmari92@gmail.com>
2013-09-27 22:34:56 +0200

    working symptom feedback, consistant functionality

--- js/script.js
+++ js/script.js
@@ -1,104 +1,104 @@
-﻿var question = type = id = "";
+﻿    var question = type = id = "";
 
-window.onload = function () {
-    //splash();
-    initialize();
+    window.onload = function () {
+        //splash();
+        initialize();
 
-    };
+        };
 
-function splash() {
-    $("#splash").delay(2000).effect("puff", 500);
-}
+    function splash() {
+        $("#splash").delay(2000).effect("puff", 500);
+    }
 
-function rscroll() {
-    $('#symptoms').animate({
-        scrollLeft: '+=20'
-    }, 50, rscroll);
-}
+    function rscroll() {
+        $('#symptoms').animate({
+            scrollLeft: '+=20'
+        }, 50, rscroll);
+    }
 
-function lscroll() {
-    $('#symptoms').animate({
-        scrollLeft: '-=20'
-    }, 50, lscroll);
-}
+    function lscroll() {
+        $('#symptoms').animate({
+            scrollLeft: '-=20'
+        }, 50, lscroll);
+    }
 
-$(function () {
+    $(function () {
 
-    $("#symptom").hover(function () {
-        document.getElementById("patientH1").innerHTML = "Symptoms";
-    });
-    $("#factor").hover(function () {
-        document.getElementById("patientH1").innerHTML = "Risk Factors";
-    });
-    $("#life").hover(function () {
-        document.getElementById("patientH1").innerHTML = "Lifestyle";
-    });
+        $("#symptom").hover(function () {
+            document.getElementById("patientH1").innerHTML = "Symptoms";
+        });
+        $("#factor").hover(function () {
+            document.getElementById("patientH1").innerHTML = "Risk Factors";
+        });
+        $("#life").hover(function () {
+            document.getElementById("patientH1").innerHTML = "Lifestyle";
+        });
 
-    $("#symptom").click(function () {
-        $("#symptoms").delay(200).fadeIn();
-        $("#choice").fadeOut();
-    });
+        $("#symptom").click(function () {
+            $("#symptoms").delay(200).fadeIn();
+            $("#choice").fadeOut();
+        });
 
-    $("#life").click(function () {
-        $("#lifestyle").delay(200).fadeIn();
-        $("#choice").fadeOut();
-    });
+        $("#life").click(function () {
+            $("#lifestyle").delay(200).fadeIn();
+            $("#choice").fadeOut();
+        });
 
-    $("#backbutton").click(function () {
-        $("#symptoms, #factors, #lifestyle").delay(200).fadeOut();
-        $("#choice").delay(200).fadeIn();
-    });
+        $("#backbutton").click(function () {
+            $("#symptoms, #factors, #lifestyle").delay(200).fadeOut();
+            $("#choice").delay(200).fadeIn();
+        });
 
-    var clicked = clicked2 = false;
-    $("#nearest").click(function () {
-        if (!clicked) {
-            $("#directions").fadeIn();
-            clicked = true;
-        }
-        else {
-            $("#directions").fadeOut();
-            clicked = false;
-        }
-    });
+        var clicked = clicked2 = false;
+        $("#nearest").click(function () {
+            if (!clicked) {
+                $("#directions").fadeIn();
+                clicked = true;
+            }
+            else {
+                $("#directions").fadeOut();
+                clicked = false;
+            }
+        });
 
-    $("#extra").click(function () {
+        $("#extra").click(function () {
+            if (!clicked2) {
+                $("#definition").fadeIn();
+                clicked2 = true;
+            }
+            else {
+                $("#definition").fadeOut();
+                clicked2 = false;
+            }
+        });
+
+        $("#symptomsresultsC").on('click','#cont',function () {
+            $("#symptomsresultsC").fadeOut();
+            $("#advisor").animate({ "height": "47%" }, 1000, "easeInOutCirc");
+            $("#menu").fadeIn();
+            $("#patient").animate({ "bottom": "2.55%" }, 1000, "easeInOutCirc");
+        });
+
+        $('.next').on('mouseenter', rscroll);
+        $('.prev').on('mouseenter', lscroll);
+        $('.next,.prev').on('mouseleave', function () {
+            $('#symptoms').stop();
+        });
+
+        /*    window.oncontextmenu = function () {
+
         if (!clicked2) {
-            $("#definition").fadeIn();
-            clicked2 = true;
+        $("#definition").fadeIn();
+        clicked2 = true;
+        $.get('servlet', { symptom: "extra" }, function (responseText) {
+        $('#definition').text(responseText);
+        });
         }
         else {
-            $("#definition").fadeOut();
-            clicked2 = false;
+        $("#definition").fadeOut();
+        clicked2 = false;
         }
+        return false;
+        }
+        */
     });
-
-    $("#symptomsresultsC").on('click','#cont',function () {
-        $("#symptomsresultsC").fadeOut();
-        $("#advisor").animate({ "height": "47%" }, 1000, "easeInOutCirc");
-        $("#menu").fadeIn();
-        $("#patient").animate({ "bottom": "2.55%" }, 1000, "easeInOutCirc");
-    });
-
-    $('.next').on('mouseenter', rscroll);
-    $('.prev').on('mouseenter', lscroll);
-    $('.next,.prev').on('mouseleave', function () {
-        $('#symptoms').stop();
-    });
-
-    /*    window.oncontextmenu = function () {
-
-    if (!clicked2) {
-    $("#definition").fadeIn();
-    clicked2 = true;
-    $.get('servlet', { symptom: "extra" }, function (responseText) {
-    $('#definition').text(responseText);
-    });
-    }
-    else {
-    $("#definition").fadeOut();
-    clicked2 = false;
-    }
-    return false;
-    }
-    */
-});
